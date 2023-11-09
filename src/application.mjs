307fe81fa0212d441b76c34321706b9d3b11d2a5
Jonathan Parker <joparker@penguinrandomhouse.com>
2023-11-09 08:20:07 -0500

    Initial content

---
+++ src/application.mjs
@@ -0,0 +1,22 @@
+/**
+ * (#)application.mjs   1.0.0   11/09/2023
+ *
+ * Copyright (c) Jonathan M. Parker
+ * 324 Lantana Drive
+ * Owings Mills, MD  21117 U.S.A
+ * All Rights Reserved.
+ */
+
+/**
+ * The application class.
+ */
+class Application {
+    /**
+     * The run method.
+     */
+    run() {
+        console.log('Hello');
+    }
+}
+
+export { Application };
