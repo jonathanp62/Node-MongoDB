994291864093f0a7925a35992ea919647da59f55
Jonathan Parker <jonathanp62@gmail.com>
2023-11-11 06:42:33 -0500

    Rewrote the local demo to use functional composition

--- src/local_demo.mjs
+++ src/local_demo.mjs
@@ -30,52 +30,37 @@ class LocalDemo {
      * The run method.
      */
     run() {
-        this.firstQuery();
-        this.secondQuery();
+        start(this._config)
+            .then(() => queryOne(this._config, this._mongoClient))
+            .then(() => queryTwo(this._config, this._mongoClient))
+            .then(() => this._mongoClient.close())
+            .then(() => console.log(`Disconnected from ${this._config.mongo.uri}/${this._config.mongo.database}`))
+            .catch(console.dir);
     }
+}
 
-    /**
-     * Connect, query, and close.
-     */
-    firstQuery() {
-        async function query(config, mongoClient) {
-            try {
-                const database = mongoClient.db(config.mongo.database);
-                const collection = database.collection(config.collections.demo);
-                const filter = {prodId: 100};
-                const item = await collection.findOne(filter);
+async function start(config) {
+    console.log(`Connected to ${config.mongo.uri}/${config.mongo.database}`);
+    console.log(`From the ${config.collections.demo} collection`);
+}
 
-                console.log(`1st: Connected to ${config.mongo.uri}/${config.mongo.database}`);
-                console.log(`From the ${config.collections.demo} collection`);
-                console.log(item);
-            } finally {
-                await mongoClient.close();
-                console.log(`1st: Disconnected from ${config.mongo.uri}/${config.mongo.database}`);
-            }
-        }
+async function queryOne(config, mongoClient) {
+    const database = mongoClient.db(config.mongo.database);
+    const collection = database.collection(config.collections.demo);
+    const filter = {prodId: 100};
+    const item = await collection.findOne(filter);
 
-        query(this._config, this._mongoClient).catch(console.dir);
-    }
+    console.log(item);
+}
 
-    secondQuery() {
-        async function query(config, mongoClient) {
-            try {
-                console.log(`2nd: Connected to ${config.mongo.uri}/${config.mongo.database}`);
+async function queryTwo(config, mongoClient) {
+    const database = mongoClient.db(config.mongo.database);
+    const collection = database.collection(config.collections.demo);
+    const filter = {};
+    const items = await collection.find(filter);
 
-                const database = mongoClient.db(config.mongo.database);
-                const collection = database.collection(config.collections.demo);
-                const filter = {};
-                const items = await collection.find(filter);
-
-                for await (const item of items) {
-                    console.log(item);
-                }
-            } finally {
-                mongoClient.close().then(() => console.log(`2nd: Disconnected from ${config.mongo.uri}/${config.mongo.database}`));
-            }
-        }
-
-        query(this._config, this._mongoClient).catch(console.dir);
+    for await (const item of items) {
+        console.log(item);
     }
 }
 
